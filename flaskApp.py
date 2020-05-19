0626efa3c332765a8782f810ed6f2ee44193fd64
RoshanB392 <roshanbonde30@gmail.com>
2020-05-20 01:23:06 +0530

    database started

--- flaskApp.py
+++ flaskApp.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from form import RegistrationForm, LoginForm
+from models imoport User, Grievance
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '30aafab764c6161f72402799774df65e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 @app.route("/home")
