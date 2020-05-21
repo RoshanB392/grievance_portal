df57c83b2d2f5d352306f4f06ec9045cede43fe3
RoshanB392 <roshanbonde30@gmail.com>
2020-05-22 01:32:38 +0530

    profile picture upload working

--- flaskApp/form.py
+++ flaskApp/form.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskApp.models import User
@@ -39,3 +41,29 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
 
     submit = SubmitField('Login')
+
+
+# Update Account Form
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+     validators=[DataRequired(), Length(min=5, max=20)])
+
+    email = StringField('Email',
+     validators=[DataRequired(), Email()])
+
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('The Username is taken. Please choose a different Username.')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('The Email is already taken. Please choose another Email.')
+
--- flaskApp/route.py
+++ flaskApp/route.py
@@ -1,6 +1,9 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, flash, redirect, url_for, request
 from flaskApp import app, bcrypt , db
-from flaskApp.form import RegistrationForm, LoginForm
+from flaskApp.form import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskApp.models import User, Grievance
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -53,9 +56,35 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pic', picture_fn)
 
-@app.route("/account")
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data) 
+            current_user.image_file = picture_file 
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your accout has been Updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pic/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
