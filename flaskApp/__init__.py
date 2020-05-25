520c06af33a59d51fd32567967d1c502250d6930
RoshanB392 <roshanbonde30@gmail.com>
2020-05-26 02:25:02 +0530

    reset email and password in progress

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -12,5 +14,15 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USER_TLS'] = True
+# app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+# app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
+app.config['MAIL_USERNAME'] = 'robinbond2k18@gmail.com'
+app.config['MAIL_PASSWORD'] = 'r0bin@2k18'
+mail = Mail(app)
+
 
 from flaskApp import route
--- flaskApp/form.py
+++ flaskApp/form.py
@@ -74,3 +74,20 @@ class PostGrievanceForm(FlaskForm):
     content = TextAreaField('Content', validators=[DataRequired()])
     grievance_picture = FileField('Uplaod Image', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Post')
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
